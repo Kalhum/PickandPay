73acef3970e6efc5b54c1dc92208aaad01c144d3
Kalhum <129973108+Kalhum@users.noreply.github.com>
2025-02-04 13:28:23 +0700

    remove debugShowCheckedModeBanner

--- pick_and_pay/lib/main.dart
+++ pick_and_pay/lib/main.dart
@@ -11,6 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Splash_screen());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: Splash_screen());
   }
 }
